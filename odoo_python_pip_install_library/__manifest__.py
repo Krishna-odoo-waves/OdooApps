fe4831b0341e29a540df242a9f3fa5cca26204f7
Krishna <odoowaves.solution@gmail.com>
2023-11-25 22:07:04 +0530

    [ADD] Odoo Pip Installer

---
+++ odoo_python_pip_install_library/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Odoo Python Pip Command Installer",
+    "summary": """Python Library Installer """,
+    "category": "Extra Tools",
+    "version": "16.0.1.0.0",
+    "sequence": 2,
+    "author": "Odoo Waves",
+    "license": "LGPL-3",
+    "website": "",
+    "description": """Install The Library With In The Odoo Environment in easily, directly and better way""",
+    "data": [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+        'wizard/pip_command_view.xml'
+    ],
+    "images": ['static/description/banner.gif'],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "price":60,
+    "currency":'USD'
+}
